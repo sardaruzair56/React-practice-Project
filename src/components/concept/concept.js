6865b301d6a00fbd004dadd4d4b9018f5b587a07
sardaruzair56 <116259618+sardaruzair56@users.noreply.github.com>
2023-09-26 16:44:15 +0500

    React practice project

---
+++ src/components/concept/concept.js
@@ -0,0 +1,13 @@
+function Concept(props) {
+  return (
+    <div>
+       <li className="concept">
+       <img src={props.image} alt={props.title} />
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </li>
+    </div>
+  )
+}
+
+export default Concept
